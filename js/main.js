37859575b8e130619fba16a93524e74b603ba6db
Veronica Bierhals <veronicabierhals1@gmail.com>
2024-03-22 18:33:25 -0300

    refatorando

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('.slider-principal').slick({
       dots: false,
       infinite: true,
-      speed: 300,
+      speed: 500,
       slidesToShow: 1,
       adaptiveHeight: true,
       autoplay: true,
