b76a393bd92657af2db58d8edfa6eae5c4bfedd7
Veronica Bierhals <veronicabierhals1@gmail.com>
2024-07-23 20:29:13 -0300

    correção para identificar paginas

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,9 @@ export default defineConfig({
         additionalData: `@import 'slick-carousel/slick/slick.css'; @import 'slick-carousel/slick/slick-theme.css';`
       }
     }
-  }
+  },
+  build: {
+    outDir: 'dist'
+  },
+  base: '/ecoestudiodesign/'
 })
