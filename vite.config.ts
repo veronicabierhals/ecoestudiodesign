c7ae8d7c6c33a9bb7f30be28461b0a30155869c4
Veronica Bierhals <veronicabierhals1@gmail.com>
2024-07-28 14:19:36 -0300

    ajustes para rodar local e no gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true
-  },
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import 'slick-carousel/slick/slick.css'; @import 'slick-carousel/slick/slick-theme.css';`
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    server: {
+      open: true
+    },
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import 'slick-carousel/slick/slick.css'; @import 'slick-carousel/slick/slick-theme.css';`
+        }
       }
-    }
-  },
-  base: '/'
+    },
+    base: command === 'build' ? '/ecoestudiodesign/' : '/'
+  }
 })
