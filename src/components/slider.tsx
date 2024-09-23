e2befb589dd64f415f29776f1ee89ebf73b346ea
Veronica Bierhals <veronicabierhals1@gmail.com>
2024-09-23 12:36:08 -0300

    alteração imagem slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Carousel } from 'react-bootstrap'
 
 const Slider: React.FC = () => {
-  const carouselItems = [{ src: '/img/banner1.png', alt: 'Slide 1' }]
+  const carouselItems = [{ src: '/img/banner1.jpg', alt: 'Slide 1' }]
 
   return (
     <Carousel className='container largura-pagina custom-carousel banner pt-4'>
